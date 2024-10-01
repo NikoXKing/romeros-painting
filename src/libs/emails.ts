77745e698c2255f107f69ea5229bd0079b22ec91
NikoXKing <nikodiaz2003@gmail.com>
2024-10-01 15:43:51 +0200

    email contact

---
+++ src/libs/emails.ts
@@ -0,0 +1,31 @@
+import nodemailer, { type SendMailOptions } from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'nikolay.envialia@gmail.com',
+        pass: import.meta.env.EMAIL_GOOGLE_API,
+    },
+})
+
+export const sendEmailQuote = async (email: string, fullName: string, message: string) => {
+    const emailQuote: SendMailOptions = {
+        from: 'nikolay.envialia@gmail.com',
+        to: 'omar@romerospaintingmaintenance.com',
+        subject: 'Quote from WebSite',
+        html: `
+        <p>Ha llegado un email de: ${fullName}</p>
+        <p>Con el siguiente email: ${email}</p>
+        <p>${message}</p>`
+    }
+
+    try {
+        await transporter.sendMail(emailQuote);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
+    }
+}
