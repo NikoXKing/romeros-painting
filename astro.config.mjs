e85215a91d0b3c28b39f81d0ede58db005cd006e
NikoXKing <nikodiaz2003@gmail.com>
2024-11-15 19:12:59 +0100

    add sitemap to page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,17 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind()],
+  site: 'https://romerospaintingmaintenance.com',
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => page !== 'https://romerospaintingmaintenance.com/portfolio'
+    })
+  ],
   adapter: vercel()
 });
